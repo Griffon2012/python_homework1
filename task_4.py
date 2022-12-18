2ed8b8acb2176601066ca7a63be970d00d4ea5f0
Grisha <trifonov-ga@yandex.ru>
2022-12-19 00:38:53 +0300

    added task_4

---
+++ task_4.py
@@ -0,0 +1,16 @@
+# Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек 
+# в этой четверти (x и y).
+
+number = int(input('Введите номер четверти: '))
+
+if number == 1:
+    result = 'x > 0, y > 0'
+elif number == 2:
+    result = 'x < 0, y > 0'
+elif number == 3:
+    result = 'x < 0, y < 0'
+elif number == 4:
+    result = 'x > 0, y < 0'
+else:
+    result = 'Нет такой четверти'
+print(result)
