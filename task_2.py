a8afd8601ad6f5b2d14b0072718b49a2d60092c3
Grisha <trifonov-ga@yandex.ru>
2022-12-19 00:54:29 +0300

    task_5

--- task_2.py
+++ task_2.py
@@ -1,9 +1,10 @@
-# Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех 
+# Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех
 # значений предиката.
 
-for x in range(0,2):
-    for y in range(0,2):
-        for z in range(0,2):
-            var1 = not(bool(x) or bool(y) or bool(z)) 
-            var2 = not(bool(x)) and not(bool(y)) and not(bool(z))
-            print(f'¬(X ⋁ Y ⋁ Z) = {var1}, ¬X ⋀ ¬Y ⋀ ¬Z = {var2} итог {var1 == var2}')
+for x in range(0, 2):
+    for y in range(0, 2):
+        for z in range(0, 2):
+            var1 = not (bool(x) or bool(y) or bool(z))
+            var2 = not (bool(x)) and not (bool(y)) and not (bool(z))
+            print(
+                f'¬(X ⋁ Y ⋁ Z) = {var1}, ¬X ⋀ ¬Y ⋀ ¬Z = {var2} итог {var1 == var2}')
